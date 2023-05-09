c4e13b043f1087967cb94e28f17591694ad5befb
Jando Becker <jando@MacBook-Air-de-Jando.local>
2023-05-09 11:30:08 -0600

    segunda  revision

--- index.ts
+++ index.ts
@@ -24,23 +24,41 @@ console.log(randomElement);
 const numbers = [1,2,3,4,5] as const
 
 // Create a function that receives an array of numbers as const, then type the returned type values sorted
-// YOUR
-// CODE
-// HERE
 
+function ordenarNumeros(numeros: readonly number[]): readonly number[] {
+    return [...numeros].sort((a, b) => a - b);
+  }
+  
+  
+  const numerosOrdenados = ordenarNumeros(numbers);
+  console.log(numerosOrdenados);
 
+  
 const value = true
 //  create that doesnt receive any parameter type, but infer the type of the returned value
 
-// YOUR
-// CODE
-// HERE
+function getValue() {
+    return value;
+  }
+
+const result = getValue();
 
 //  create a function that infer the type of the returned value, but receives a parameter type, if the parameter type exist,
 //  use it to determine the returned type, if not, use the type of the given value
 //  you can check the definition od useSatate in react to see how it works.
 
-// YOUR
-// CODE
-// HERE
+function getType(type?: any): any {
+    if (type) {
+      return type;
+    } else {
+      return typeof value;
+    }
+  }
+
+
+
+const result1 = getType();
+console.log(typeof result1); 
+
+  
 
